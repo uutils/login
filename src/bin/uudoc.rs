7ac98e663389f87ec698c832b71bf9669c4dd4f8
Sylvestre Ledru <sylvestre@debian.org>
2024-03-26 22:44:00 +0100

    update of the headers

--- src/bin/uudoc.rs
+++ src/bin/uudoc.rs
@@ -1,4 +1,4 @@
-// This file is part of the uutils procps package.
+// This file is part of the uutils login package.
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
