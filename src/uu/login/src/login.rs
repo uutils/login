d79b65f9e8933bb9d617c39a613c74dd7a758fa1
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2025-12-30 10:28:10 +0100

    Adapt to changes in uucore

--- src/uu/login/src/login.rs
+++ src/uu/login/src/login.rs
@@ -5,10 +5,7 @@
 
 use clap::crate_version;
 use clap::Command;
-use uucore::{error::UResult, format_usage, help_about, help_usage};
-
-const ABOUT: &str = help_about!("login.md");
-const USAGE: &str = help_usage!("login.md");
+use uucore::{error::UResult, format_usage};
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
@@ -17,9 +14,13 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
 }
 
 pub fn uu_app() -> Command {
+    const USAGE: &str = "login [-p] [-h host] [username] [ENV=VAR...]
+login [-p] [-h host] -f username
+login [-p] -r host";
+
     Command::new(uucore::util_name())
         .version(crate_version!())
-        .about(ABOUT)
+        .about("begin session on the system")
         .override_usage(format_usage(USAGE))
         .infer_long_args(true)
     // TODO
